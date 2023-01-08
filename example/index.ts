f990a2a8edc75493dc606af6b97f8335e7fb5c76
Daniele Fedeli <daniele.fedeli@kaleyra.com>
2023-01-08 17:08:04 +0100

    chore - Add example

---
+++ example/index.ts
@@ -0,0 +1,27 @@
+import lyra, { insertWithHooks } from '@lyrasearch/lyra';
+import { afterInsertAdvancedQuery } from '../src/hooks/after-insert';
+import { advancedSearch } from '../src/index';
+
+async function init() {
+  const db = lyra.create({
+    schema: {
+      author: 'string',
+      alive: 'boolean',
+      age: 'number',
+    },
+    hooks: {
+      afterInsert: afterInsertAdvancedQuery,
+    },
+  });
+
+  await insertWithHooks(db, { author: 'Daniele', alive: true, age: 25 });
+  await insertWithHooks(db, { author: 'Daniele', alive: false, age: 25 });
+
+  const result = advancedSearch(db, {
+    where: { alive: true },
+    term: 'Daniele',
+  });
+  // Retrieve only the first document.
+}
+
+init();
