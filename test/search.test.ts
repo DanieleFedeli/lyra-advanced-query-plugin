f07f1cc6e3984e4fcc2dbff3e7b1830bb9ec1462
Daniele Fedeli <daniele.fedeli@kaleyra.com>
2023-01-08 13:50:45 +0100

    feat - Calling underlying lyra search method

--- test/search.test.ts
+++ test/search.test.ts
@@ -3,7 +3,7 @@ import { advancedSearch } from 'src'
 import { afterInsertAdvancedQuery } from 'src/hooks/after-insert'
 import t from 'tap' 
 
-t.test('Advanced search' , async t => {
+t.test('Advanced search with right term' , async t => {
   const lyra = create({
     schema: {
       'author': 'string',
@@ -16,14 +16,70 @@ t.test('Advanced search' , async t => {
   })
 
   const idDead = await insertWithHooks(lyra, { author: 'Dead', 'alive': false, booksSold: 1233 })
-  const idAlive = await insertWithHooks(lyra, {author: 'Alive', 'alive': true, booksSold: 123}) 
-  
-  const ids = advancedSearch(lyra, {
+  const idAlive1 = await insertWithHooks(lyra, {author: 'Alive', 'alive': true, booksSold: 123}) 
+  const idAlive2 = await insertWithHooks(lyra, {author: 'Alive', 'alive': true, booksSold: 1234})
+
+  const resultsAlive = advancedSearch(lyra, {
     where: { alive: true },
     term: 'alive'
   })
 
-  t.same(ids.size, 1)
-  t.ok(ids.has(idAlive.id))
+  t.same(resultsAlive.hits.length, 2)
+  t.same(resultsAlive.hits[0].id, idAlive1.id)
+
+  const resultsDead = advancedSearch(lyra, {where: {alive: false}, term: 'Dead'});
+  t.same(resultsDead.hits.length, 1);
+  t.same(resultsDead.hits[0].id, idDead.id);
+ 
+  t.end()
+})
+
+t.test('Advanced search with right term but where query produces 1 results' , async t => {
+  const lyra = create({
+    schema: {
+      'author': 'string',
+      'alive': 'boolean',
+      'booksSold': 'number'
+    },
+    hooks: {
+      afterInsert: afterInsertAdvancedQuery
+    }
+  })
+
+  await insertWithHooks(lyra, { author: 'Alive', 'alive': false, booksSold: 1233 })
+  await insertWithHooks(lyra, {author: 'Alive', 'alive': true, booksSold: 123}) 
+
+  const resultsAlive = advancedSearch(lyra, {
+    where: { alive: true },
+    term: 'Alive'
+  })
+
+  t.same(resultsAlive.hits.length, 1)
+
+  t.end()
+})
+
+t.test('Advanced search with numbers' , async t => {
+  const lyra = create({
+    schema: {
+      'author': 'string',
+      'alive': 'boolean',
+      'booksSold': 'number'
+    },
+    hooks: {
+      afterInsert: afterInsertAdvancedQuery
+    }
+  })
+
+  await insertWithHooks(lyra, { author: 'Alive', 'alive': false, booksSold: 1233 })
+  await insertWithHooks(lyra, {author: 'Alive', 'alive': true, booksSold: 123}) 
+
+  const resultsAlive = advancedSearch(lyra, {
+    where: { booksSold: {'=': 1233} },
+    term: 'Alive'
+  })
+
+  t.same(resultsAlive.hits.length, 1)
+
   t.end()
 })
