0db8fafd44eba1b7827d5d11190e4dac646bd612
Daniele Fedeli <daniele.fedeli@kaleyra.com>
2023-01-08 16:47:59 +0100

    chore/add-linter

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,14 +1,17 @@
-export function pick<TObject extends Record<string, unknown>>(obj: TObject, properties: string[]){
-    const picked = Object.create(null);
+export function pick<TObject extends Record<string, unknown>>(
+  obj: TObject,
+  properties: string[]
+) {
+  const picked = Object.create(null);
 
-    const objKeys = Object.keys(obj);
-    const _properties = objKeys.length > properties.length ? properties : objKeys;
+  const objKeys = Object.keys(obj);
+  const _properties = objKeys.length > properties.length ? properties : objKeys;
 
-    for(const property of _properties) {
-        // Not supporting nested properties yet
-        if(property.includes('.')) continue;
-        picked[property] = obj[property];   
-    }
+  for (const property of _properties) {
+    // Not supporting nested properties yet
+    if (property.includes('.')) continue;
+    picked[property] = obj[property];
+  }
 
-    return picked;
+  return picked;
 }
--- test/set.test.ts
+++ test/set.test.ts
@@ -1,35 +1,34 @@
 import { setIntersection } from 'src/utils/set';
 import t from 'tap';
 
-t.test('Set intersection', t=> {
-  
-  t.test('Should return the second set if the first one is nullish', t=> {
+t.test('Set intersection', (t) => {
+  t.test('Should return the second set if the first one is nullish', (t) => {
     const a = new Set();
     const b = new Set();
     b.add(1);
 
-    t.same(setIntersection(a, b).entries(), b.entries() )
-    t.end()
-  })
+    t.same(setIntersection(a, b).entries(), b.entries());
+    t.end();
+  });
 
-  t.test('Should return the first set if the second one is nullish', t => {
+  t.test('Should return the first set if the second one is nullish', (t) => {
     const b = new Set();
     const a = new Set();
     a.add(1);
 
-    t.same(setIntersection(a, b).entries(), a.entries())
-    t.end()
-  })
+    t.same(setIntersection(a, b).entries(), a.entries());
+    t.end();
+  });
 
-  t.test('Should return the element in common', t => {
+  t.test('Should return the element in common', (t) => {
     const b = new Set([1, 2, 3, 7]);
     const a = new Set([2, 3, 4]);
 
-    const result = setIntersection(a, b)
-    const expected = [2,3]
-    t.same([...result], expected)
-    t.end()
-  })
+    const result = setIntersection(a, b);
+    const expected = [2, 3];
+    t.same([...result], expected);
+    t.end();
+  });
 
-  t.end()
-})
+  t.end();
+});
